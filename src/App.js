0012b699904c3960da993a952d238b1d514cb500
tursunbek <tiku_kg@list.ru>
2021-08-27 13:34:19 +0600

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './components/header/header';
+import PostCard from "./components/header/postCard";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  render(){
+    return (
+        <>
+          <Header/>
+          <PostCard/>
+        </>
+    )
+  }
 }
 
 export default App;
